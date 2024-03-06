fa1a9bc2937713c786c790ddbcd822ee1d6e200a
Filipe Lima <fhemrique55@gmail.com>
2024-03-06 17:35:05 -0300

    Criando Projeto

---
+++ estudosTestes/src/main/java/com/estudosTestes/EstudosTestesApplication.java
@@ -0,0 +1,13 @@
+package com.estudosTestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosTestesApplication.class, args);
+	}
+
+}
