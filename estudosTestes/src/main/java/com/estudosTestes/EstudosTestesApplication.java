8e1a3bc0e2957e63dc9fa9286a516afd47effdd1
Filipe Lima <fhemrique55@gmail.com>
2024-03-06 18:59:22 -0300

    Cenários de Teste

--- estudosTestes/src/main/java/com/estudosTestes/EstudosTestesApplication.java
+++ estudosTestes/src/main/java/com/estudosTestes/EstudosTestesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EstudosTestesApplication {
 
     public static void main(String[] args) {
-        //TESTE DE UNIDADE
+        //CENÁRIOS DE TESTE
         SpringApplication.run(EstudosTestesApplication.class, args);
     }
 }
